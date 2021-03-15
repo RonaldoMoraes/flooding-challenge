8ea6f86dd517ef3867a17850a27985ff92fe47d1
Ronaldo Moraes <ronaldo.moraes1990@gmail.com>
2021-03-14 22:26:00 -0300

    Detalhe bobo, melhoria de lógica.

--- index.php
+++ index.php
@@ -9,11 +9,9 @@
             include_once __DIR__ . '/utils.php';
             require_once __DIR__ . '/vendor/autoload.php';
 
+            include_once __DIR__ . '/get.php';
             if (isPost()) {
-                include_once __DIR__ . '/get.php';
                 include_once __DIR__ . '/execute.php';
-            } else {
-                include_once __DIR__ . '/get.php';
             }
             ?>
         </div>
